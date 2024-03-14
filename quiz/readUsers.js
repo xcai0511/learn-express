276f03b75078da967402e075004bc3427bddfb27
Xiaojin Cai <xcai0511@gmail.com>
2024-03-14 14:19:33 -0400

    read users

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,23 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+    let usernames = req.users.map(function(user) {
+        return {id: user.ikd, username: user.username};
+    });
+    res.send(usernames);
+})
+router.get('/username/:name', (req, res) => {
+    let name = req.params.name;
+    let user_with_name = req.users.filter(function(user) {
+        return user.username === name;
+    });
+    if (user_with_name.length) {
+        res.json(user_with_name);
+    } else {
+        res.status(404).send('User not found');
+    }
+});
+
+
+module.exports = router;
