ce05589634e5280cc660040e2a6fa3f8c0189bb0
Xiaojin Cai <xcai0511@gmail.com>
2024-03-14 14:24:18 -0400

    write user

--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -2,3 +2,19 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const router = express.Router()
+
+router.post('/adduser', (req, res) => {
+    let newuser = req.body;
+    req.users.push(newuser);
+    fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users, null, 2), (err) => {
+        if (err) {
+            console.log('Failed to write', err);
+            return res.status(500).send('Failed to add user');
+        }
+        console.log('User saved');
+        res.send('User added successfully');
+    });
+    res.send('done');
+})
+
+module.exports = router;
